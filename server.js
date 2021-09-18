a1930cc3e2b6806e795aa5f7bcf90c698270b57c
Kellie Matteson <kelliematteson@yahoo.com>
2021-09-18 14:03:16 -0700

    working on mongoose

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = 3000;
+
+
+//Body Parser Middleware below to give access to req.body
+app.use(express.urlencoded({extended: true }));
+app.use(express.json());
+app.use((req, res, next ) => {
+    console.log(req.body)
+    next()
+});
+
+//connect to mongoose
+mongoose.connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true
+})
+
+mongoose.connection.once('connected', () => ('Mongoose is ready!'));
+
+//Port Listener
+app.listen(PORT, () =>{
+    console.log(`listening on port ${PORT}`)
+});
